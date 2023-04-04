b0f1811cb5feed7a3b11ba67daa7c2af512b4269
amiola <agumiola@gmail.com>
2023-04-04 16:19:49 -0300

    skills/nav media queries

--- src/navbar/Nav.jsx
+++ src/navbar/Nav.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import './Nav.css'
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
     <nav>
@@ -10,12 +11,17 @@ const Nav = () => {
         <h1 className="nav-name">agustín miola</h1>
         <h1 className="nav-fe">front-end web developer</h1>
         </div>
-        <ul className='nav-links'>
+        <ul className={`nav-links ${isOpen? 'open':''}`}>
             <li><NavLink to='/home' className='nav-home'>home</NavLink></li>
             <li><NavLink to='/skills' className='nav-skills'>skills</NavLink></li>
             <li><NavLink to='/projects' className='nav-projects'>projects</NavLink></li>
             <li><NavLink to='/contact' className='nav-contact'>contact</NavLink></li>
         </ul>
+        <div className="nav-hamburguer" onClick={()=> setIsOpen(!isOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
     </nav>
     </>
   )
