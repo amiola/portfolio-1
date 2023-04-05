236a120a1f63febc0944141d67d5d49366bd9534
amiola <agumiola@gmail.com>
2023-04-04 23:05:32 -0300

    build 3- change image path

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -26,8 +26,8 @@ const Home = () => {
         </div>
         <div className="contact-box"><NavLink to='/contact' className="contact-title">contact</NavLink></div>
         <div className="social-links">
-          <NavLink to='https://www.linkedin.com/in/agust%C3%ADn-eli%C3%A1n-miola-649264211/' ><img src="../public/icons/linkedin.png" alt="linedin link" className='linkedin-link'/></NavLink>
-            <NavLink to='https://github.com/amiola' ><img src="../public/icons/github.png" alt="github link" className='git-link'/></NavLink>
+          <NavLink to='https://www.linkedin.com/in/agust%C3%ADn-eli%C3%A1n-miola-649264211/' ><img src="public\icons\linkedin.png" alt="linedin link" className='linkedin-link'/></NavLink>
+            <NavLink to='https://github.com/amiola' ><img src="public\icons\github.png" alt="github link" className='git-link'/></NavLink>
         </div>
     </section>
     </div>
