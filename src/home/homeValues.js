f0f72233a5ff979c5cba5e374a1b14901677b0bd
amiola <agumiola@gmail.com>
2023-04-27 11:38:01 -0300

    9months

--- src/home/homeValues.js
+++ src/home/homeValues.js
@@ -1,18 +1,18 @@
 export const homeValues = [
     {
-        about: 'Hi, my name is Agustín! I\'ve been studying Front-End Web Development for the last 8 months.  Take a look of what I made at',
+        about: 'Hi, my name is Agustín! I\'ve been studying Front-End Web Development for the last 9 months.  Take a look of what I made at',
         projects: 'projects',
         skills: 'skills',
         contact: 'contact'
     },
     {
-        about: 'Olá, meu nome é Agustín! Eu tenho estudado Desenvolvimento Web Front-End nos últimos 8 meses. Dá uma olhada no que eu fiz em',
+        about: 'Olá, meu nome é Agustín! Eu tenho estudado Desenvolvimento Web Front-End nos últimos 9 meses. Dá uma olhada no que eu fiz em',
         projects: 'projetos',
         skills: 'habilidades',
         contact: 'contato'
     },
     {
-        about: '¡Hola, mi nombre es Agustín! He estado estudiando Desarrollo Web Front-End durante los últimos 8 meses. Mira los trabajos que hice en',
+        about: '¡Hola, mi nombre es Agustín! He estado estudiando Desarrollo Web Front-End durante los últimos 9 meses. Mira los trabajos que hice en',
         projects: 'proyectos',
         skills: 'habilidades',
         contact: 'contacto'
